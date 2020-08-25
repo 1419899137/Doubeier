a9ce660c0b3c5422d90b4ade482d095232940a47
doubeier <1419899137@qq.com>
2020-08-25 14:59:08 +0800

    1

---
+++ src/main/java/com/luomu/doubeier/DoubeierApplication.java
@@ -0,0 +1,13 @@
+package com.luomu.doubeier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubeierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubeierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luomu/doubeier/DoubeierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luomu.doubeier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubeierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
